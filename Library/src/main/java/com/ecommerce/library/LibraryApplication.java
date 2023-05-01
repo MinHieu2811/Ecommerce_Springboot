86dec6f60954668eadd7684e75f632dff1b791ad
W317 <liemvu202@gmail.com>
2023-05-01 15:37:16 +0700

    test

--- Library/src/main/java/com/ecommerce/library/LibraryApplication.java
+++ Library/src/main/java/com/ecommerce/library/LibraryApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LibraryApplication {
 
+
+
     public static void main(String[] args) {
         SpringApplication.run(LibraryApplication.class, args);
     }
