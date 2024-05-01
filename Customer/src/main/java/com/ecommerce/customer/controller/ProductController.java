823526e5cd73ea86c125b010f6eeabf6797c0ce9
giang <truonggiang542002@gmail.com>
2024-05-01 12:24:38 +0700

    update code

--- Customer/src/main/java/com/ecommerce/customer/controller/ProductController.java
+++ Customer/src/main/java/com/ecommerce/customer/controller/ProductController.java
@@ -1,38 +1,49 @@
 package com.ecommerce.customer.controller;
 import com.ecommerce.library.dto.CategoryDto;
+import com.ecommerce.library.dto.ProductDto;
 import com.ecommerce.library.model.Category;
 import com.ecommerce.library.model.Product;
 import com.ecommerce.library.service.CategoryService;
 import com.ecommerce.library.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
 @Controller
 public class ProductController {
-
+    private static final int PAGE_SIZE = 8;
     @Autowired
     private ProductService productService;
 
     @Autowired
     private CategoryService categoryService;
 
-    @GetMapping("/products")
-    public String products(Model model){
+    @GetMapping("/products/{pageNo}")
+    public String products(@PathVariable("pageNo") int pageNo, Model model){
         List<CategoryDto> categoryDtoList = categoryService.getCategoryAndProduct();
-        List<Product> products = productService.getAllProducts();
         List<Product> listViewProducts = productService.listViewProducts();
         model.addAttribute("categories", categoryDtoList);
         model.addAttribute("viewProducts", listViewProducts);
-        model.addAttribute("products", products);
+        model.addAttribute("customerDto", null);
+
+        Page<ProductDto> productPage = productService.pageProducts(pageNo);
+        model.addAttribute("title", "Manage Product");
+        model.addAttribute("size", productPage.getSize());
+        model.addAttribute("totalPages", productPage.getTotalPages());
+        model.addAttribute("currentPage", pageNo);
+        model.addAttribute("productsPage", productPage);
+
         return "shop";
     }
 
-    @GetMapping("/find-product/{id}")
+    @GetMapping("/product/{id}")
     public String findProductById(@PathVariable("id") Long id, Model model){
         Product product = productService.getProductById(id);
         Long categoryId = product.getCategory().getId();
@@ -42,16 +53,25 @@ public class ProductController {
         return "product-detail";
     }
 
-    @GetMapping("/products-in-category/{id}")
-    public String getProductsInCategory(@PathVariable("id") Long categoryId ,Model model){
+    @GetMapping("/products-in-category/{id}/{pageNo}")
+    public String getProductsInCategory(@PathVariable("id") Long categoryId, @PathVariable("pageNo") int pageNo, Model model){
         Category category = categoryService.findById(categoryId);
         List<CategoryDto> categories = categoryService.getCategoryAndProduct();
         List<Product> products = productService.getProductsInCategory(categoryId);
         model.addAttribute("category",category);
         model.addAttribute("categories", categories);
         model.addAttribute("products", products);
+
+        Page<Product> productPage = productService.getProductsInCategory(categoryId, PageRequest.of(pageNo, PAGE_SIZE));
+        model.addAttribute("title", "Manage Product");
+        model.addAttribute("size", productPage.getSize());
+        model.addAttribute("totalPages", productPage.getTotalPages());
+        model.addAttribute("currentPage", pageNo);
+        model.addAttribute("productsPage", productPage);
+
         return "products-in-category";
     }
+
 
     @GetMapping("/high-price")
     public String filterHighPrice(Model model){
@@ -75,5 +95,4 @@ public class ProductController {
         model.addAttribute("categories", categories);
         return "filter-low-price";
     }
-
 }
--- Library/src/main/java/com/ecommerce/library/service/ProductService.java
+++ Library/src/main/java/com/ecommerce/library/service/ProductService.java
@@ -3,6 +3,7 @@ package com.ecommerce.library.service;
 import com.ecommerce.library.dto.ProductDto;
 import com.ecommerce.library.model.Product;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
@@ -31,6 +32,8 @@ public interface ProductService {
     List<Product> getRelatedProducts(Long categoryId);
 
     List<Product> getProductsInCategory(Long categoryId);
+
+    Page<Product> getProductsInCategory(Long categoryId, Pageable pageable);
 
     List<Product> filterHighPrice();
 
